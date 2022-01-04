aa32754bbeb7f1b8ca2d3ce2982f36fa9daede91
Austin Huang <gengminghuang@gmail.com>
2022-01-04 12:28:56 +0900

    Prettier config

--- src/components/editablePage.js
+++ src/components/editablePage.js
@@ -1,8 +1,22 @@
-import { v4 as uuidv4 } from 'uuid';
-import React from "react";
+import { v4 as uuidv4 } from 'uuid'
+import React, { useState } from 'react'
 
-const initialBlock = { id: uuidv4(), html: "", tag: "p" }
+const initialBlock = { id: uuidv4(), html: '', tag: 'p' }
 
 const EditablePage = () => {
-    
+  const blocks = useState([initialBlock])
+
+  return (
+    <div className="Page">
+      {blocks.map((block, key) => {
+        return (
+          <div key={key} id={block.id}>
+            Tag: {block.tag}, Content: {block.html}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
+
+export default EditablePage
